af3040d9b46dcd7a07a18e5fc5afdb839f20f0b0
Quique Borredá <qborreda@gmail.com>
2017-02-25 22:49:30 +0100

    Incanto project detail page with working carousel; lacks second carusel and big one

--- js/main.js
+++ js/main.js
@@ -16,4 +16,35 @@ $( document ).ready(function() {
   $(window).on('scroll', floatedHeader);
   floatedHeader();
 
+  // Project Carrousel
+  $('.js-carrusel').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: $('.btn-arrows-left'),
+    nextArrow: $('.btn-arrows-right'),
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 980,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
+
 });
