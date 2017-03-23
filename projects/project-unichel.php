b5b63141343dec4e2f92b884dbbce5b6f22564c1
Quique Borredá <qborreda@gmail.com>
2017-03-23 19:52:01 +0100

    Project details for Unichel

---
+++ projects/project-unichel.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="description" content="VM Guru — консалтинговое агентство визуальных коммуникаций в фэшн ритейле, специализирующееся на коммерческом визуальном мерчандайзинге, оформлении витрин, дизайне магазинов, а также обучении в данных сегментах.">
+    <meta name="keywords" content="vmguru, visual merchandising, визуальный мерчандайзинг, витринистика, визуальный мерчандайзинг обучение, консалтинг по визуальному мерчандайзингу, обучение мерчендайзингу, курсы для мерчендайзеров, мерчандайзинг образование">
+    <title>Визуальный мерчандайзинг , Витринистика, Дизайн магазинов - консалтинг и курсы в VM Guru</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+
+<body class="tmpl-trainings">
+    <!-- MAIN HEADER -->
+    <?php include "../header.php"; ?>
+
+    <div class="wrapper">
+
+        <div class="stripped-title">
+            <span></span>
+            <p>ПРОЕКТЫ</p>
+        </div>
+        <section class="project-details">
+
+            <article class="hex-content clearfix">
+                <div class="hex-content-left">
+                    <p class="hex-content-name">РАЗРАБОТКА СТАНДАРТОВ
+                    <br/>И СОЗДАНИЕ МЕРЧАНДАЙЗИНГ
+                    <br/>БУК «ЮНИЧЕЛ»</p>
+                    <div class="hex-content-image">
+                        <img src="/imgs/bkgr_hex_project_modis1.jpg" alt="" />
+                    </div>
+                </div>
+                <div class="hex-content-right">
+                    <p class="hex-content-name">РАЗРАБОТКА СТАНДАРТОВ
+                    <br/>И СОЗДАНИЕ МЕРЧАНДАЙЗИНГ
+                    <br/>БУК «ЮНИЧЕЛ»</p>
+                    <p class="hex-content-description"></p>
+                    <ul class="hex-content-list">
+                        <li>Пошаговая разработка и тестирование стандартов визуального мерчандайзинга бренда с «нуля»:
+                            <ul>
+                                <li>Инструмент I – СТАНДАРТЫ ВЫКЛАДКИ ТОВАРА В ТОРГОВОМ ЗАЛЕ;</li>
+                                <li>Инструмент II – КОММЕРЧЕСКОЕ ЗОНИРОВАНИЕ;</li>
+                                <li>Инструмент III – СТАНДАРТЫ ОФОРМЛЕНИЕ РАСПРОДАЖИ.</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <p class="hex-content-description">РАЗРАБОТКА КОМПЛЕКТА POS МАТЕРИАЛОВ SALE ДЛЯ МАГАЗИНОВ РОЗНИЧНОЙ СЕТИ «ЮНИЧЕЛ»</p>
+                    <div class="hex-content-client-description">Производитель обуви с 1932г. Торговая сеть включает более 500 фирменных магазинов. <a href="http://www.unichel.ru/" target="_blank">www.unichel.ru</a></div>
+                </div>
+            </article>
+
+            <div class="project-carrusel">
+
+                <div class="js-carrusel-1">
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-01.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-02.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-03.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-04.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-05.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-06.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-07.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-08.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-09.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-10.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-11.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-12.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-13.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-14.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-15.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-1-16.jpg" alt=""></div>
+                </div>
+                
+                <img class="btn-arrows btn-arrows-left" src="/imgs/btn_arrow_left.png" />
+
+                <img class="btn-arrows btn-arrows-right" src="/imgs/btn_arrow_right.png" />
+            </div>
+
+            <div class="carrusel-legend">/ проект реализован: ноябрь 2015 - май 2016 / выборочные страницы отчетных документов /</div>
+
+        </section>
+
+        <section class="project-details">
+
+            <article class="hex-content clearfix">
+                <div class="hex-content-left">
+                    <p class="hex-content-name">ВНЕДРЕНИЕ СТАНДАРТОВ ВМ В РОЗНИЧНУЮ СЕТЬ;
+                    <br/>РАЗРАБОТКА СИСТЕМЫ ДИСТАНЦИОННОГО
+                    <br/>ВИЗУАЛЬНОГО МЕРЧАНДАЙЗИНГА<sup>*</sup>
+                    <br/>СЕТИ «ЮНИЧЕЛ»</p>
+                    <div class="hex-content-image">
+                        <img src="/imgs/bkgr_hex_project_modis2.jpg" alt="" />
+                    </div>
+                </div>
+                <div class="hex-content-right">
+                    <p class="hex-content-name">ВНЕДРЕНИЕ СТАНДАРТОВ ВМ В РОЗНИЧНУЮ СЕТЬ;
+                    <br/>РАЗРАБОТКА СИСТЕМЫ ДИСТАНЦИОННОГО
+                    <br/>ВИЗУАЛЬНОГО МЕРЧАНДАЙЗИНГА<sup>*</sup>
+                    <br/>СЕТИ «ЮНИЧЕЛ»</p>
+                    <p class="hex-content-description"></p>
+                    <ul class="hex-content-list">
+                        <li>Разработка структуры и тайминга внедрения проекта</li>
+                        <li>Проведение цикла Модел Стор проектов экспертом А. Баландиной 1 раз в сезон при «заходе» сезонных коллекций с практическим обучением сотрудников сети</li>
+                        <li>Cоздание сетевых инструкций по текущей сезонной презентации коллекций по всем линиям для всех магазинов сети «ЮНИЧЕЛ» с адаптацией под сетевые форматы</li>
+                        <li>Обучение, отбор и аттестация ключевых специалистов бренда вовлеченных в процесс ВМ в 12 ключевых регионах России</li>
+                    </ul>
+                    <p class="hex-content-description small"><sup>*</sup>Система Дистанционного ВМ – обязательное условие эффективной работы сетевого бренда. Данная практика позволяет отстроить систему визуального мерчандайзинга сетевого бренда, оптимизировать и эффективно управлять ею в дальнейшем. В основе системы дистанционного ВМ — Moдел Стор проект (МС) — цикличный проект по созданию «идеального» торгового зала и витрин в типовом магазине бренда с последующим транслированием проекта на всю сеть посредством презентаций-инструкций, обучения персонала и системы обратной отчетности.</p>
+                </div>
+            </article>
+
+            <div class="project-carrusel">
+
+                <div class="js-carrusel-2">
+                    <div class="carr-item"><img src="/imgs/projects/modis-2-01.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-2-02.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-2-03.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-2-04.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-2-05.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-2-06.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-2-07.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-2-08.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-2-09.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-2-10.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/modis-2-11.jpg" alt=""></div>
+                </div>
+
+                <img class="btn-arrows btn-arrows-left" src="/imgs/btn_arrow_left.png" />
+
+                <img class="btn-arrows btn-arrows-right" src="/imgs/btn_arrow_right.png" />
+            </div>
+
+            <div class="carrusel-legend">/ проект реализован: ноябрь 2016 – по наст. время (раз в сезон) / выборочные страницы Модел Стор презентаций /</div>
+
+        </section>
+
+        <!-- FOOTER -->
+        <?php include "../footer.php"; ?>
+
+    </div>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+<script src="/js/main.js" type="text/javascript" charset="utf-8"></script>
+
+</html>
