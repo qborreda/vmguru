9dbcecfc1e659ec592b46c7ab885967c8b6a0038
Quique Borredá <qborreda@gmail.com>
2017-04-17 18:29:54 +0200

    Project details for Sneznaia Koroleva

---
+++ projects/project-sneznaia-koroleva.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="description" content="VM Guru — консалтинговое агентство визуальных коммуникаций в фэшн ритейле, специализирующееся на коммерческом визуальном мерчандайзинге, оформлении витрин, дизайне магазинов, а также обучении в данных сегментах.">
+    <meta name="keywords" content="vmguru, visual merchandising, визуальный мерчандайзинг, витринистика, визуальный мерчандайзинг обучение, консалтинг по визуальному мерчандайзингу, обучение мерчендайзингу, курсы для мерчендайзеров, мерчандайзинг образование">
+    <title>Визуальный мерчандайзинг , Витринистика, Дизайн магазинов - консалтинг и курсы в VM Guru</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+
+<body class="tmpl-trainings">
+    <!-- MAIN HEADER -->
+    <?php include "../header.php"; ?>
+
+    <div class="wrapper">
+
+        <div class="stripped-title">
+            <span></span>
+            <p>ПРОЕКТЫ</p>
+        </div>
+        <section class="project-details">
+
+            <article class="hex-content clearfix">
+                <div class="hex-content-left">
+                    <p class="hex-content-name">РАЗРАБОТКА СТАНДАРТОВ 
+                    <br/>ПРЕЗЕНТАЦИИ АКСЕССУАРОВ
+                    <br/>В МАГАЗИНАХ «СНЕЖНАЯ КОРОЛЕВА» 
+                    <br/>КОРПОРАТИВНОЕ ВМ ОБУЧЕНИЕ</p>
+                    <div class="hex-content-image">
+                        <img src="/imgs/bkgr_hex_project_sneznaia1.jpg" alt="" />
+                    </div>
+                </div>
+                <div class="hex-content-right">
+                    <p class="hex-content-name">РАЗРАБОТКА СТАНДАРТОВ 
+                    <br/>ПРЕЗЕНТАЦИИ АКСЕССУАРОВ 
+                    <br/>В МАГАЗИНАХ «СНЕЖНАЯ КОРОЛЕВА» 
+                    <br/>КОРПОРАТИВНОЕ ВМ ОБУЧЕНИЕ</p>
+                    <p class="hex-content-description">проект включает:</p>
+                    <ul class="hex-content-list">
+                        <li>Экспертный аудит стандартов презентации товара в аксессуарной зоне магазинов «Снежная Королева»
+                        <li>Отчетная ВМ инструкция «Стандарты презентации аксессуаров в магазинах «Снежная Королева»</li>
+                        <li>Тренинг-программа по обучению ВМ стандартам презентации с ремерчандайзингом акс. зон типовых магазинов для команды ВМ бренда</li>
+                        <li>Сетевая инструкция по сезонной презентации аксессуаров</li>
+                    </ul>
+                    <div class="hex-content-client-description">«Снежная Королева» — российская сеть мультибрендовых магазинов модной одежды; более 100 магазинов</div>
+                </div>
+            </article>
+
+            <div class="project-carrusel">
+
+                <div class="js-carrusel-1">
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-1-01.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-1-02.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-1-03.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-1-04.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-1-05.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-1-06.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-1-07.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-1-08.jpg" alt=""></div>
+                </div>
+                
+                <img class="btn-arrows btn-arrows-left" src="/imgs/btn_arrow_left.png" />
+
+                <img class="btn-arrows btn-arrows-right" src="/imgs/btn_arrow_right.png" />
+            </div>
+
+            <div class="carrusel-legend">/ проект реализован: март 2013 / выборочные страницы отчетных презентаций /</div>
+
+        </section>
+
+        <section class="project-details">
+
+            <article class="hex-content clearfix">
+                <div class="hex-content-left">
+                    <p class="hex-content-name">СЕЗОННЫЙ МОДЕЛ СТОР ПРОЕКТ 
+                    <br/>«ОФОРМЛЕНИЕ ВИТРИН И ВХОДНЫХ ЗОН»
+                    <br/>КОРПОРАТИВНОЕ ВМ ОБУЧЕНИЕ</p>
+                    <div class="hex-content-image">
+                        <img src="/imgs/bkgr_hex_project_sneznaia2.jpg" alt="" />
+                    </div>
+                </div>
+                <div class="hex-content-right">
+                    <p class="hex-content-name">СЕЗОННЫЙ МОДЕЛ СТОР ПРОЕКТ 
+                    <br/>«ОФОРМЛЕНИЕ ВИТРИН И ВХОДНЫХ ЗОН»
+                    <br/>КОРПОРАТИВНОЕ ВМ ОБУЧЕНИЕ</p>
+                    <p class="hex-content-description">проект включает:</p>
+                    <ul class="hex-content-list">
+                        <li>Экспертный аудит стандартов оформления витрин и входной зоны в магазинах «Снежная Королева»</li>
+                        <li>Сезонное оформление пилотных витрин с тренинг-практикумом для команды ВМ бренда</li>
+                        <li>Сетевая презентация по текущему оформлению магазинов сети</li>
+                        <li>Отчетная презентация «Рекомендации по оптимизации стандартов презентации товара в витринах» для руководства бренда</li>
+                    </ul>
+                </div>
+            </article>
+
+            <div class="project-carrusel">
+
+                <div class="js-carrusel-2">
+                    <!-- <div class="carr-item"><img src="/imgs/projects/sneznaia-2-01.jpg" alt=""></div> -->
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-2-02.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-2-03.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-2-04.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-2-05.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-2-06.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-2-07.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/sneznaia-2-08.jpg" alt=""></div>
+                </div>
+
+                <img class="btn-arrows btn-arrows-left" src="/imgs/btn_arrow_left.png" />
+
+                <img class="btn-arrows btn-arrows-right" src="/imgs/btn_arrow_right.png" />
+            </div>
+
+            <div class="carrusel-legend">/ проект реализован: март 2013 / выборочные страницы отчетных документов /</div>
+
+        </section>
+
+        <!-- FOOTER -->
+        <?php include "../footer.php"; ?>
+
+    </div>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+<script src="/js/main.js" type="text/javascript" charset="utf-8"></script>
+
+</html>
