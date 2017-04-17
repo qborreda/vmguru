aa3cb9b28160c9a71e969072b459965d70f64cb6
Quique Borredá <qborreda@gmail.com>
2017-04-17 16:50:10 +0200

    Changes the dates of trainings

--- _training-events.php
+++ _training-events.php
@@ -1,18 +1,6 @@
 <!-- TRAINING EVENTS -->
 <div class="training-items">
     <article class="event-article">
-        <a class="no-border" href="/visual-merchandising-dlia-magazinov-obuvi-training.php">
-        <img src="imgs/bkgr_event3.jpg">
-        <div class="article-stripe">
-            <div class="article-dates">
-                <span class="month">апреля</span>
-                <span class="date">11—12</span>
-            </div>
-            <p>Визуальный мерчандайзинг для магазинов обуви и аксессуаров</p>
-        </div>
-        </a>
-    </article>
-    <article class="event-article">
         <a class="no-border" href="/kommercheskii-visual-merchandising-training.php">
         <img src="imgs/bkgr_event2.jpg">
         <div class="article-stripe">
@@ -24,18 +12,6 @@
         </div></a>
     </article>
     <article class="event-article">
-        <a class="no-border" href="/sozdanie-magazine-fashion-segments-training.php">
-        <img src="imgs/bkgr_event1.jpg">
-        <div class="article-stripe">
-            <div class="article-dates">
-                <span class="month">мая</span>
-                <span class="date">15</span>
-            </div>
-            <p>Cоздание эффективного магазина фэшн сегмента: планировки, зонирование, оптимальная вместимость, ВМ-дизайн</p>
-        </div>
-        </a>
-    </article>
-    <article class="event-article">
         <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php">
         <img src="imgs/bkgr_event4.jpg">
         <div class="article-stripe">
@@ -44,6 +20,30 @@
                 <span class="date">16—17</span>
             </div>
             <p>Витринистика: создание эффективных витрин. Большой эффект без больших расходов.</p>
+        </div>
+        </a>
+    </article>
+    <article class="event-article">
+        <a class="no-border" href="/sozdanie-magazine-fashion-segments-training.php">
+        <img src="imgs/bkgr_event1.jpg">
+        <div class="article-stripe">
+            <div class="article-dates">
+                <span class="month">мая</span>
+                <span class="date">18</span>
+            </div>
+            <p>Cоздание эффективного магазина фэшн сегмента: планировки, зонирование, оптимальная вместимость, ВМ-дизайн</p>
+        </div>
+        </a>
+    </article>
+    <article class="event-article">
+        <a class="no-border" href="/visual-merchandising-dlia-magazinov-obuvi-training.php">
+        <img src="imgs/bkgr_event3.jpg">
+        <div class="article-stripe">
+            <div class="article-dates">
+                <span class="month">июня</span>
+                <span class="date">5—6</span>
+            </div>
+            <p>Визуальный мерчандайзинг для магазинов обуви и аксессуаров</p>
         </div>
         </a>
     </article>
--- visual-merchandising-dlia-magazinov-obuvi-training.php
+++ visual-merchandising-dlia-magazinov-obuvi-training.php
@@ -32,8 +32,8 @@
                 <div class="box-skewy"></div>
                 <div class="box-texts">
                     <div class="box-texts-date">
-                        <span class="day">11—12</span>
-                        <span class="month">апреля</span>
+                        <span class="day">5—6</span>
+                        <span class="month">июня</span>
                     </div>
                     <div class="box-texts-title">
                         ВИЗУАЛЬНЫЙ МЕРЧАНДАЙЗИНГ ДЛЯ МАГАЗИНОВ ОБУВИ И АКСЕССУАРОВ
