09139248d488f10b9e289ddd14282d88e0085b3a
Quique Borredá <qborreda@gmail.com>
2017-12-17 13:09:43 +0100

    Created a common client grid to be easily updated. Added new client logo

--- training.php
+++ training.php
@@ -110,40 +110,7 @@
         </div> -->
         <section class="consulting-section2">
             <h2 style="margin:10px auto;">У НАС ОБУЧАЛИСЬ:</h2>
-            <div class="client-grid">
-                <span class="client-grid-logo"><img src="imgs/client-koroneva.jpg" alt="Koroneva"></span>
-                <span class="client-grid-logo"><img src="imgs/client-modis.jpg" alt="Modis"></span>
-                <span class="client-grid-logo"><img src="imgs/client-qxt.jpg" alt="QXT"></span>
-                <span class="client-grid-logo"><img src="imgs/client-ikunuka.jpg" alt="Ikonika"></span>
-                <span class="client-grid-logo"><img src="imgs/client-incanto.jpg" alt="Incanto"></span>
-                <span class="client-grid-logo"><img src="imgs/client-unicel.jpg" alt="Unicel"></span>
-                <span class="client-grid-logo"><img src="imgs/client-baon.jpg" alt="Baon"></span>
-                <span class="client-grid-logo"><img src="imgs/client-butikru.jpg" alt="Butik.ru"></span>
-                <span class="client-grid-logo"><img src="imgs/client-eleganzza.jpg" alt="Eleganzza"></span>
-                <span class="client-grid-logo"><img src="imgs/client-marccain.jpg" alt="Marccain"></span>
-                <span class="client-grid-logo"><img src="imgs/client-sela.jpg" alt="Sela"></span>
-                <span class="client-grid-logo"><img src="imgs/client-mexx.jpg" alt="Mexx"></span>
-                <span class="client-grid-logo"><img src="imgs/client-tomfarr.jpg" alt="Tom Farr"></span>
-                <span class="client-grid-logo"><img src="imgs/client-orteka.jpg" alt="Orteka"></span>
-                <span class="client-grid-logo"><img src="imgs/client-steilmann.jpg" alt="Steilmann"></span>
-
-                <span class="client-grid-logo"><img src="imgs/client-univermag.jpg" alt="Univermag"></span>
-                <span class="client-grid-logo"><img src="imgs/client-ekonoaic.jpg" alt="Ekopoaic"></span>
-                <span class="client-grid-logo"><img src="imgs/client-calvinklein.jpg" alt="Calvin Klein Jeans"></span>
-                <span class="client-grid-logo"><img src="imgs/client-gloriajeans.jpg" alt="Gloria Jeans"></span>
-                <span class="client-grid-logo"><img src="imgs/client-converse.jpg" alt="Converse"></span>
-                <span class="client-grid-logo"><img src="imgs/client-ilquattro.jpg" alt="il Quattro"></span>
-                <span class="client-grid-logo"><img src="imgs/client-normann.jpg" alt="Normann"></span>
-                <span class="client-grid-logo"><img src="imgs/client-sintagamma.jpg" alt="Sinta Gamma"></span>
-                <span class="client-grid-logo"><img src="imgs/client-axara.jpg" alt="Axara"></span>
-
-                <span class="client-grid-logo"><img src="imgs/client-pablosky.jpg" alt="Pablosky"></span>
-                <span class="client-grid-logo"><img src="imgs/client-markformelle.jpg" alt="Mark Formelle"></span>
-                <span class="client-grid-logo"><img src="imgs/client-veravictoriavito.jpg" alt="Vera Victoria Vito"></span>
-                <span class="client-grid-logo"><img src="imgs/client-dinoricci.jpg" alt="Dino Ricci"></span>
-                <span class="client-grid-logo"><img src="imgs/client-dijeans.jpg" alt="Di Jeans"></span>
-                <span class="client-grid-logo"><img src="imgs/client-mreason.jpg" alt="M. Reason"></span>
-            </div>
+            <?php include "_client-grid.php"; ?>
         </section>
         <section class="event-section5">
             <h2 style="margin:10px auto;">УЗНАТЬ ПОДРОБНЕЙ / ЗАКАЗАТЬ УСЛУГУ:</h2>
