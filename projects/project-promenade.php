ab805eeb0b823670fe68c9da3ab063e9a8b6e668
Quique Borredá <qborreda@gmail.com>
2018-03-11 13:31:19 +0100

    Added project Promenade, text and hex images

---
+++ projects/project-promenade.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="description" content="VM Guru — консалтинговое агентство визуальных коммуникаций в фэшн ритейле, специализирующееся на коммерческом визуальном мерчандайзинге, оформлении витрин, дизайне магазинов, а также обучении в данных сегментах.">
+    <meta name="keywords" content="vmguru, visual merchandising, визуальный мерчандайзинг, витринистика, визуальный мерчандайзинг обучение, консалтинг по визуальному мерчандайзингу, обучение мерчендайзингу, курсы для мерчендайзеров, мерчандайзинг образование">
+    <title>Визуальный мерчандайзинг , Витринистика, Дизайн магазинов - консалтинг и курсы в VM Guru</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+
+<body class="tmpl-trainings">
+    <!-- MAIN HEADER -->
+    <?php include "../header.php"; ?>
+
+    <div class="wrapper">
+
+        <div class="stripped-title">
+            <span></span>
+            <p>ПРОЕКТЫ</p>
+        </div>
+        <section class="project-details">
+
+            <article class="hex-content clearfix">
+                <div class="hex-content-left">
+                    <p class="hex-content-name">РАЗРАБОТАКА ДИЗАЙН-ПРОЕКТА
+                    </br/>ТИПОВОГО МАГАЗИНА;
+                    <br/>ТЕХНИЧЕСКОЕ СОПРОВОЖДЕНИЕ ПРОЕКТА</p>
+                    <div class="hex-content-image">
+                        <img src="/imgs/bkgr_hex_project_promenade1.jpg" alt="" />
+                    </div>
+                </div>
+                <div class="hex-content-right">
+                    <p class="hex-content-name">РАЗРАБОТАКА ДИЗАЙН-ПРОЕКТА
+                    </br/>ТИПОВОГО МАГАЗИНА;
+                    <br/>ТЕХНИЧЕСКОЕ СОПРОВОЖДЕНИЕ ПРОЕКТА</p>
+                    <p class="hex-content-description">Проект включает разработку и визуализацию следующих составляющих:</p>
+                    <ul class="hex-content-list">
+                        <li>ПРЕ-КОНЦЕПЦИЯ/ ЭСКИЗНЫЙ ПРОЕКТ</li>
+                        <li>ПОЛНЫЙ ДИЗАЙН-ПРОЕКТ
+                            <ul>
+                                <li>Визуализация магазина в 3D</li>
+                                <li>План отделки</li>
+                                <li>План расстановки оборудования с размерами</li>
+                                <li>Развертки по стенам</li>
+                                <li>План потолка (с расстановкой размерами и наименованием осветительного оборудования)</li>
+                                <li>План полов (с размерами и наименованием отделочных материалов)</li>
+                                <li>Дизайн-проект фасада и вывески </li>
+                                <li>Дизайн-проект кассовой зоны </li>
+                                <li>Дизайн-проект торгового оборудования и зон примерочных (с размерами и материалами торгового оборудования </li>
+                                <li>Подбор декоративных ВМ элементов (манекены, торсы и пр.)</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <p class="hex-content-description">РАЗРАБОТКА ПАКЕТА ТЕХНИЧЕСКОЙ ДОКУМЕНТАЦИИ ИНЖЕНЕРНЫХ РАЗДЕЛОВ ДП</p>
+                    <div class="hex-content-client-description">Согласование тех документации с тц.</div>
+                </div>
+            </article>
+
+            <div class="project-carrusel">
+
+                <div class="js-carrusel-1">
+                    <div class="carr-item"><img src="/imgs/projects/unichel-1-01.jpg" alt=""></div>
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-1-02.jpg" alt=""></div> -->
+                    <div class="carr-item"><img src="/imgs/projects/unichel-1-03.jpg" alt=""></div>
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-1-04.jpg" alt=""></div> -->
+                    <div class="carr-item"><img src="/imgs/projects/unichel-1-05.jpg" alt=""></div>
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-1-06.jpg" alt=""></div> -->
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-1-07.jpg" alt=""></div> -->
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-1-08.jpg" alt=""></div> -->
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-1-09.jpg" alt=""></div> -->
+                    <div class="carr-item"><img src="/imgs/projects/unichel-1-10.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/unichel-1-11.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/unichel-1-12.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/unichel-1-13.jpg" alt=""></div>
+                </div>
+
+                <img class="btn-arrows btn-arrows-left" src="/imgs/btn_arrow_left.png" />
+
+                <img class="btn-arrows btn-arrows-right" src="/imgs/btn_arrow_right.png" />
+            </div>
+
+            <div class="carrusel-legend">/ дата реализации - февраль 2018 / выборочные стр. Дизайн- проекта /</div>
+
+        </section>
+
+        <section class="project-details">
+
+            <article class="hex-content clearfix">
+                <div class="hex-content-left">
+                    <p class="hex-content-name">БРЕНДИНГ-ПРОЕКТ;
+                        <br/>СОЗДАНИЕ БРЕНД БУК
+                        <br/>РАЗРАБОТКА КОМПЛЕКТА POS SALE</p>
+                    <div class="hex-content-image">
+                        <img src="/imgs/bkgr_hex_project_promenade2.jpg" alt="" />
+                    </div>
+                </div>
+                <div class="hex-content-right">
+                    <p class="hex-content-name">БРЕНДИНГ-ПРОЕКТ;
+                        <br/>СОЗДАНИЕ БРЕНД БУК
+                        <br/>РАЗРАБОТКА КОМПЛЕКТА POS SALE</p>
+                    <p class="hex-content-description">Включает в себя:</p>
+                    <ul class="hex-content-list">
+                        <li>Логотип </li>
+                        <li>Набор фирменных цветов (цвета по СMYK; параметры использования)</li>
+                        <li>Визитка магазина</li>
+                        <li>Ценник</li>
+                        <li>Пакеты</li>
+                        <li>Бонусная карта</li>
+                    </ul>
+                    <p class="hex-content-description">Комплект POS материалов Sale включает:</p>
+                    <ul class="hex-content-list">
+                        <li>Афишетки для Торгового зала: "Новая Коллекция", "Распродажа", Sale до 30%, Sale до 50%, Sale до 70% - один формат; </li>
+                        <li>Баннер "распродажа" на витрины под размер.</li>
+                    </ul>
+                    <p class="hex-content-description"></p>
+                </div>
+            </article>
+
+            <div class="project-carrusel">
+
+                <div class="js-carrusel-2">
+                    <div class="carr-item"><img src="/imgs/projects/unichel-2-01.jpg" alt=""></div>
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-2-02.jpg" alt=""></div> -->
+                    <div class="carr-item"><img src="/imgs/projects/unichel-2-03.jpg" alt=""></div>
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-2-04.jpg" alt=""></div> -->
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-2-05.jpg" alt=""></div> -->
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-2-06.jpg" alt=""></div> -->
+                    <div class="carr-item"><img src="/imgs/projects/unichel-2-07.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/unichel-2-08.jpg" alt=""></div>
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-2-09.jpg" alt=""></div> -->
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-2-10.jpg" alt=""></div> -->
+                    <div class="carr-item"><img src="/imgs/projects/unichel-2-11.jpg" alt=""></div>
+                    <!-- <div class="carr-item"><img src="/imgs/projects/unichel-2-12.jpg" alt=""></div> -->
+                    <div class="carr-item"><img src="/imgs/projects/unichel-2-13.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/unichel-2-14.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/unichel-2-15.jpg" alt=""></div>
+                </div>
+
+                <img class="btn-arrows btn-arrows-left" src="/imgs/btn_arrow_left.png" />
+
+                <img class="btn-arrows btn-arrows-right" src="/imgs/btn_arrow_right.png" />
+            </div>
+
+            <div class="carrusel-legend">/ дата реализации - февраль 2018 / выборочные стр. Брендинг- проекта /</div>
+
+        </section>
+
+        <div class="stripped-title">
+            <span></span>
+            <p>ОТЗЫВ КЛИЕНТА</p>
+        </div>
+
+        <section class="project-testimonials">
+            <div class="person">
+                <img src="/imgs/projects/unichel-testimonial.jpg" alt="Екатерина Железнякова">
+                <div class="person-text-layout">
+                    <h4 class="person-name">АЛЕКСАНДР ШАПКИН</h4>
+                    <span class="person-position bkgr-hi">Владелец бренда «Promenade»</span>
+                </div>
+            </div>
+            <div class="testimonial-text">
+                <p>Я благодарен Анне Баландиной и ее команде за качественный и быстро реализованный проект.</p>
+                <p>По сути мы получили «магазин под ключ». Команда VM Guru разработала и реализовала для нас следeющие задачи:.</p>
+
+                <p>Сейчас мы на стадии строительства первого магазина. Далее совместное открытие и ВМ обучение персонала.</p>
+                <p>Ранее прослушал тренинги Анны  "Создание эффективного магазина" и "Визуальный мерчандайзинг для магазинов обуви и аксессуаров" - очень полезны владельцам бизнеса, рекомендую. Мне они дали комплекс современных ВМ знаний и мощный толчок для ВМ изменений в моем бренде.</p>
+                <p>Желаю Vm Guru неиссякаемого креатива, бесконечного горизонта идей и крепкого финансового здоровья!</p>
+            </div>
+        </section>
+
+        <!-- FOOTER -->
+        <?php include "../footer.php"; ?>
+
+    </div>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+<script src="/js/main.js" type="text/javascript" charset="utf-8"></script>
+
+</html>
