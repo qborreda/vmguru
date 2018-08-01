cdcc482f87c71c58e4a7005193996f6ff1b1b0ce
Quique Borredá <qborreda@gmail.com>
2018-08-01 21:02:45 +0200

    Implemented Consulting items; not-responsive yet

--- _consulting-elements.php
+++ _consulting-elements.php
@@ -1,55 +1,70 @@
 <!-- CONSULTING ELEMENTS -->
 <!-- TODO: Name the different sections better -->
-<div class="training-items">
+<div class="consulting-items">
     <!-- 1 -->
-    <article class="event-article">
+    <article class="container">
         <a class="no-border" href="/visual-merchandising-dlia-magazinov-obuvi-training.php">
-        <img src="imgs/bkgr_event3.jpg">
-        <div class="article-stripe">
-            <div class="article-dates">
-                <span class="month">августа</span>
-                <span class="date">30 - 31</span>
-            </div>
-            <p>Визуальный мерчандайзинг для магазинов обуви и аксессуаров</p>
+        <img src="imgs/bkgr_skew_consulting1.jpg">
+
+        <div class="dark-stripe">
+          <div class="title">Дизайн<br />магазинов</div>
+          <ul class="bullet-list">
+              <li>полный дизайн проект</li>
+              <li>торговое оборудование</li>
+              <li>фирменный стиль бренда</li>
+              <li>комплект POS материалов (графика)</li>
+          </ul>
         </div>
+        <div class="cta-button">Узнать больше ›</div>
         </a>
     </article>
     <!-- 2 -->
-    <article class="event-article">
+    <article class="container">
         <a class="no-border" href="/kommercheskii-visual-merchandising-training.php">
-        <img src="imgs/bkgr_event2.jpg">
-        <div class="article-stripe">
-            <div class="article-dates">
-                <span class="month">сентября</span>
-                <span class="date">17 - 18</span>
-            </div>
-            <p>Коммерческий визуальный мерчандайзинг; прикладные техники «от А до Я»</p>
-        </div></a>
+        <img src="imgs/bkgr_skew_consulting2.jpg">
+
+        <div class="dark-stripe">
+          <div class="title">Стратегический<br />мерчандайзинг</div>
+          <ul class="bullet-list">
+              <li>аудит</li>
+              <li>мерчандайзинг-бук</li>
+              <li>разработка ВМ стандартов</li>
+              <li>обучение и внедрение</li>
+          </ul>
+        </div>
+        <div class="cta-button">Узнать больше ›</div>
+        </a>
     </article>
     <!-- 3 -->
-    <article class="event-article">
+    <article class="container">
         <a class="no-border" href="/sozdanie-magazine-fashion-segments-training.php">
-        <img src="imgs/bkgr_event1.jpg">
-        <div class="article-stripe">
-            <div class="article-dates">
-                <span class="month">сентября</span>
-                <span class="date">19</span>
-            </div>
-            <p>Cоздание эффективного магазина фэшн сегмента: планировки, зонирование, оптимальная вместимость, ВМ-дизайн</p>
+        <img src="imgs/bkgr_skew_consulting3.jpg">
+        <div class="dark-stripe">
+          <div class="title">Практические<br />ВМ услуги</div>
+          <ul class="bullet-list">
+            <li>выкладка товаров </li>
+            <li>мерчандайзинг при открытии магазина</li>
+            <li>разработка витринных концепций</li>
+            <li>производство декораций</li>
+            <li>оформление витрин</li>
+          </ul>
         </div>
+        <div class="cta-button">Узнать больше ›</div>
         </a>
     </article>
     <!-- 4 -->
-    <article class="event-article">
+    <article class="container">
         <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php">
-        <img src="imgs/bkgr_event4.jpg">
-        <div class="article-stripe">
-            <div class="article-dates">
-                <span class="month">сентября</span>
-                <span class="date">20 - 21</span>
-            </div>
-            <p>Витринистика: создание эффективных витрин. Большой эффект без больших расходов.</p>
+        <img src="imgs/bkgr_skew_consulting4.jpg">
+        <div class="dark-stripe">
+          <div class="title">Обучение ВМ <br />и витринистике</div>
+          <ul class="bullet-list">
+              <li>групповые тренинги</li>
+              <li>корпоративные тренинги «под бренд»</li>
+              <li>обучение арендаторовТорговых Центров</li>
+          </ul>
         </div>
+        <div class="cta-button">Узнать больше ›</div>
         </a>
     </article>
 </div>
