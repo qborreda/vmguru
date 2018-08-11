bc550846f118ea77781049d4068086c13c8bff75
Quique Borredá <qborreda@gmail.com>
2018-08-11 19:13:58 +0200

    Implemented dropdown on Seminars and Masters, plus content

--- _seminars-masters.php
+++ _seminars-masters.php
@@ -33,9 +33,79 @@
     <p class="speaker">Семинар<br />Анны Баландиной</p>
     <p class="description">Пазл для сборки эффективного магазина; как создать эффективный магазин обуви и аксессуаров и избежать ошибок с самого начала</p>
   </article>
+</div>
 
-  <div class="js-toggle-content">
-    Abrete sésamo
-  </div>
+<div class="expand-trigger js-trigger-toggle"  data-target=".expanded-seminars">
+  <span>Больше событий
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30px" viewBox="0 0 111 32" enable-background="new 0 0 111 32" xml:space="preserve">
+       <polyline fill="none" stroke-width="3" stroke-miterlimit="10" points="0.729,1.342 52.597,30.157 110.229,1.342"/>
+    </svg>
+  </span>
+</div>
 
+<!-- EXPANDED -->
+<div class="expanded-seminars js-toggle-content">
+  <article class="seminar-item">
+    <img class="logo" src="/imgs/img-seminar-cjf.jpg">
+    <p class="title">CJF – Детская Мода</p>
+    <p class="subtitle">27 сентября 15.20 – 16.20</p>
+    <p class="speaker">Семинар<br />Анны Баландиной</p>
+    <p class="description">Как создать эффективный детский магазин без максимальных затрат; планировки, зонирование, дизайн и правила ВМ</p>
+  </article>
+
+  <article class="seminar-item">
+    <img class="logo" src="/imgs/img-seminar-cjf.jpg">
+    <p class="title">CJF – Детская Мода</p>
+    <p class="subtitle">20 февраля 2018</p>
+    <p class="speaker">Семинар<br />Анны Баландиной</p>
+    <p class="description">Семь способов увеличения продаж с помощью инструментов визуального мерандайзинга</p>
+  </article>
+
+  <article class="seminar-item">
+    <img class="logo" src="/imgs/img-seminar-cpm.jpg">
+    <p class="title">Collection Premiere Moscow </p>
+    <p class="subtitle">21 февраля 2018</p>
+    <p class="speaker">Семинар<br />Анны Баландиной</p>
+    <p class="description">Дизайн магазина и торговое оборудование: как избежать ошибок с самого начала и создать эффективный магазин</p>
+  </article>
+
+  <article class="seminar-item">
+    <img class="logo" src="/imgs/img-seminar-lingerie.jpg">
+    <p class="title">Lingerie Show-Forum</p>
+    <p class="subtitle">22 февраля 14.00-14.45</p>
+    <p class="speaker">Семинар<br />Анны Баландиной</p>
+    <p class="description">Визуальный мерчандайзинг в сегменте нижнего белья - работа с эмоциями; как продавать товар на уровне подсознания</p>
+  </article>
+
+  <article class="seminar-item">
+    <img class="logo" src="/imgs/img-seminar-shoestar.jpg">
+    <p class="title">ХI выставка SHOESSTAR</p>
+    <p class="subtitle">2 марта 13.30 – 14.45</p>
+    <p class="speaker">Семинар<br />Анны Баландиной</p>
+    <p class="description">Современные ВМ технологии для магазинов обуви и аксессуаров; как поднять продажи в магазине средствами ВМ</p>
+  </article>
+
+  <article class="seminar-item">
+    <img class="logo" src="/imgs/img-seminar-british.jpg">
+    <p class="title">Британская Высшая Школа Дизайнй</p>
+    <p class="subtitle">24 мая 2018</p>
+    <p class="speaker">Семинар<br />Анны Баландиной</p>
+    <p class="description">Визуальный мерчандайзинг как инструмент эффективных продаж; как это работает?</p>
+  </article>
+
+  <article class="seminar-item">
+    <img class="logo" src="/imgs/img-seminar-fashionindustry.jpg">
+    <p class="title">Fashion Industry</p>
+    <p class="subtitle">23 марта 13.00 – 14.00</p>
+    <p class="speaker">Семинар<br />Анны Баландиной</p>
+    <p class="description">Как поднять продажи в магазине средствами визуального мерчандайзинга; современные технологии коммерческого ВМ</p>
+  </article>
+
+  <article class="seminar-item">
+    <img class="logo" src="/imgs/img-seminar-omk.jpg">
+    <p class="title">Обувь. Мир кожи-2017. Весна</p>
+    <p class="subtitle">22 февраля 14.00 – 14.45</p>
+    <p class="speaker">Семинар<br />Анны Баландиной</p>
+    <p class="description">Современные технологии визуального мерчандайзинга (ВМ) для магазинов обуви и аксессуаров. Как поднять продажи в магазине средствами ВМ?</p>
+  </article>
 </div>
