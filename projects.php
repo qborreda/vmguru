9dcad01f87a585dc4b7ab590e50ce0fd91211109
Quique Borredá <qborreda@gmail.com>
2018-10-20 16:39:36 +0300

    Projects/oscar
    * Project Oscar page done; link from Projects
    
    * Added carousel to Oscar project. optimized images.
    
    * Adds two links for project Oscar to Projects page
    
    * Further changes to order for Projects page
    
    * Re pictures for project Oscar in Projects
    
    * Changes the order of items in Projects page
    
    * Optimized image for project

--- projects.php
+++ projects.php
@@ -36,16 +36,6 @@
             <ul class="hex-grid clr">
                 <li class="hex-item" data-link="/projects/project-promenade.php">
                     <div class="white-icon">
-                        <img class="hex-content" src="/imgs/bkgr_hex_project_promenade1.jpg" alt="" />
-                        <!-- <div class="hex-title white-text">Promenade</div> -->
-                        <p class="hex-description is-little-text">
-                            дизайн-проект магазина;
-                            <br/>тех. сопровождение
-                        </p>
-                    </div>
-                </li>
-                <li class="hex-item" data-link="/projects/project-promenade.php">
-                    <div class="white-icon">
                         <img class="hex-content" src="/imgs/bkgr_hex_project_promenade2.jpg" alt="" />
                         <!-- <div class="hex-title white-text">Promenade</div> -->
                         <p class="hex-description is-little-text">
@@ -54,6 +44,16 @@
                         </p>
                     </div>
                 </li>
+                    <li class="hex-item" data-link="/projects/project-promenade.php">
+                        <div class="white-icon">
+                            <img class="hex-content" src="/imgs/bkgr_hex_project_promenade1.jpg" alt="" />
+                            <!-- <div class="hex-title white-text">Promenade</div> -->
+                            <p class="hex-description is-little-text">
+                                дизайн-проект магазина;
+                                <br/>тех. сопровождение
+                            </p>
+                        </div>
+                    </li>
                 <li class="hex-item" data-link="/projects/project-xitzona.php">
                     <div>
                         <img class="hex-content" src="/imgs/bkgr_hex_project8.jpg" alt="" />
@@ -64,17 +64,23 @@
                         </p>
                     </div>
                 </li>
-                <li class="hex-item" data-link="/projects/project-incanto.php">
+                <li class="hex-item" data-link="/projects/project-oscar.php">
                     <div>
-                        <img class="hex-content" src="/imgs/bkgr_hex_project1.jpg" alt="" />
-                        <!-- <div class="hex-title">INCANTO 1</div> --><p class="hex-description is-little-text">мерчандайзинг бук для концепта vanilla sky</p>
+                        <img class="hex-content" src="/imgs/bkgr_hex_project27.jpg" alt="" />
+                        <!-- <div class="hex-title">Oscar 1</div> -->
+                        <p class="hex-description white-text is-little-text">
+                            ДИЗАЙН-ПРОЕКТ МАГАЗИНА;
+                            <br />РАЗРАБОТКА ОБОРУДОВАНИЯ
+                        </p>
                     </div>
                 </li>
-                <li class="hex-item" data-link="/projects/project-incanto.php">
+                <li class="hex-item" data-link="/projects/project-oscar.php">
                     <div>
-                        <img class="hex-content" src="/imgs/bkgr_hex_project2.jpg" alt="" />
-                        <!-- <div class="hex-title">INCANTO 2</div> -->
-                        <p class="hex-description is-little-text">аудит торговых концептов бренда</p>
+                        <img class="hex-content" src="/imgs/bkgr_hex_project28.jpg" alt="" />
+                        <!-- <div class="hex-title">Oscar 2</div> -->
+                        <p class="hex-description white-text is-little-text">
+                            СЕЗОННЫЙ РЕМЕРЧАНДАЙЗИНГ И ОБУЧЕНИЕ
+                        </p>
                     </div>
                 </li>
                 <li class="hex-item" data-link="/projects/project-unichel.php">
@@ -105,29 +111,17 @@
                         </p>
                     </div>
                 </li>
-
-                <li class="hex-item"  data-link="/projects/project-modis.php">
-                    <div class="white-icon">
-                        <img class="hex-content" src="/imgs/bkgr_hex_project6.jpg" alt="" />
-                        <!-- <div class="hex-title">Modis 1</div> -->
-                        <p class="hex-description white-text">
-                            АУДИТ СТАНДАРТОВ ВМ;
-                            <br/>РЕДРЕССИНГ МАГАЗИНОВ С
-                            <br/>ЗАМЕРОМ ЭФФЕКТИВНОСТИ;
-                            <br/>РЕКОМЕНДАЦИИ ПО ОПТИМИЗАЦИИ
-                            <br/>ВМ СТАНДАРТОВ
-                        </p>
+                <li class="hex-item" data-link="/projects/project-incanto.php">
+                    <div>
+                        <img class="hex-content" src="/imgs/bkgr_hex_project1.jpg" alt="" />
+                        <!-- <div class="hex-title">INCANTO 1</div> --><p class="hex-description is-little-text">мерчандайзинг бук для концепта vanilla sky</p>
                     </div>
                 </li>
-                <li class="hex-item" data-link="/projects/project-modis.php">
+                <li class="hex-item" data-link="/projects/project-incanto.php">
                     <div>
-                        <img class="hex-content" src="/imgs/bkgr_hex_project7.jpg" alt="" />
-                        <!-- <div class="hex-title">Modis 2</div> -->
-                        <p class="hex-description">
-                            РАЗРАБОТКА И ВНЕДРЕНИЕ
-                            <br/>ПРОЕКТА MODEL STORE – ВИТРИНЫ И «ГОРЯЧИЕ ЗОНЫ»;
-                            <br/>КОРПОРАТИВНОЕ ОБУЧЕНИЕ
-                        </p>
+                        <img class="hex-content" src="/imgs/bkgr_hex_project2.jpg" alt="" />
+                        <!-- <div class="hex-title">INCANTO 2</div> -->
+                        <p class="hex-description is-little-text">аудит торговых концептов бренда</p>
                     </div>
                 </li>
                 <li class="hex-item" data-link="/projects/project-sportdepo.php">
@@ -159,24 +153,26 @@
                         </p>
                     </div>
                 </li>
-                <li class="hex-item" data-link="/projects/project-sneznaia-koroleva.php">
-                    <div>
-                        <img class="hex-content" src="/imgs/bkgr_hex_project9.jpg" alt="" />
-                        <!-- <div class="hex-title">Sneznaia 1</div> -->
-                        <p class="hex-description">
-                            РАЗРАБОТКА СТАНДАРТОВ ПРЕЗЕНТАЦИИ
-                            <br/>АКСЕССУАРОВ;
-                            <br/>КОРПОРАТИВНОЕ ОБУЧЕНИЕ
+                <li class="hex-item"  data-link="/projects/project-modis.php">
+                    <div class="white-icon">
+                        <img class="hex-content" src="/imgs/bkgr_hex_project6.jpg" alt="" />
+                        <!-- <div class="hex-title">Modis 1</div> -->
+                        <p class="hex-description white-text">
+                            АУДИТ СТАНДАРТОВ ВМ;
+                            <br/>РЕДРЕССИНГ МАГАЗИНОВ С
+                            <br/>ЗАМЕРОМ ЭФФЕКТИВНОСТИ;
+                            <br/>РЕКОМЕНДАЦИИ ПО ОПТИМИЗАЦИИ
+                            <br/>ВМ СТАНДАРТОВ
                         </p>
                     </div>
                 </li>
-                <li class="hex-item" data-link="/projects/project-sneznaia-koroleva.php">
+                <li class="hex-item" data-link="/projects/project-modis.php">
                     <div>
-                        <img class="hex-content" src="/imgs/bkgr_hex_project10.jpg" alt="" />
-                        <!-- <div class="hex-title">Sneznaia 2</div> -->
+                        <img class="hex-content" src="/imgs/bkgr_hex_project7.jpg" alt="" />
+                        <!-- <div class="hex-title">Modis 2</div> -->
                         <p class="hex-description">
                             РАЗРАБОТКА И ВНЕДРЕНИЕ
-                            <br/>ПРОЕКТА MODEL STORE – ВИТРИНЫ;
+                            <br/>ПРОЕКТА MODEL STORE – ВИТРИНЫ И «ГОРЯЧИЕ ЗОНЫ»;
                             <br/>КОРПОРАТИВНОЕ ОБУЧЕНИЕ
                         </p>
                     </div>
@@ -212,6 +208,28 @@
                             МЕРЧАНДАЙЗИНГ БУК;
                             <br/>РАЗРАБОТКА И ВНЕДРЕНИЕ  ДИСТАНЦИОННОГО ВМ;
                             <br/>СЕЗОННАЯ ПОДДЕРЖКА И ОБУЧЕНИЕ
+                        </p>
+                    </div>
+                </li>
+                <li class="hex-item" data-link="/projects/project-sneznaia-koroleva.php">
+                    <div>
+                        <img class="hex-content" src="/imgs/bkgr_hex_project9.jpg" alt="" />
+                        <!-- <div class="hex-title">Sneznaia 1</div> -->
+                        <p class="hex-description">
+                            РАЗРАБОТКА СТАНДАРТОВ ПРЕЗЕНТАЦИИ
+                            <br/>АКСЕССУАРОВ;
+                            <br/>КОРПОРАТИВНОЕ ОБУЧЕНИЕ
+                        </p>
+                    </div>
+                </li>
+                <li class="hex-item" data-link="/projects/project-sneznaia-koroleva.php">
+                    <div>
+                        <img class="hex-content" src="/imgs/bkgr_hex_project10.jpg" alt="" />
+                        <!-- <div class="hex-title">Sneznaia 2</div> -->
+                        <p class="hex-description">
+                            РАЗРАБОТКА И ВНЕДРЕНИЕ
+                            <br/>ПРОЕКТА MODEL STORE – ВИТРИНЫ;
+                            <br/>КОРПОРАТИВНОЕ ОБУЧЕНИЕ
                         </p>
                     </div>
                 </li>
---
+++ projects/project-oscar.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="description" content="VM Guru — консалтинговое агентство визуальных коммуникаций в фэшн ритейле, специализирующееся на коммерческом визуальном мерчандайзинге, оформлении витрин, дизайне магазинов, а также обучении в данных сегментах.">
+    <meta name="keywords" content="vmguru, visual merchandising, визуальный мерчандайзинг, витринистика, визуальный мерчандайзинг обучение, консалтинг по визуальному мерчандайзингу, обучение мерчендайзингу, курсы для мерчендайзеров, мерчандайзинг образование">
+    <title>Визуальный мерчандайзинг , Витринистика, Дизайн магазинов - консалтинг и курсы в VM Guru</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+
+<body class="tmpl-trainings">
+    <!-- MAIN HEADER -->
+    <?php include "../header.php"; ?>
+
+    <div class="short_wrapper">
+
+        <section class="new-section">
+          <div class="section-title">Проекты</div>
+        </section>
+        <section class="project-details">
+
+            <article class="hex-content clearfix">
+                <div class="hex-content-left">
+                    <p class="hex-content-name">РАЗРАБОТАКА ДИЗАЙН-ПРОЕКТА
+                        <br />ФЛАГМАНСКОГО МАГАЗИНА OSCAR;
+                        <br />РАЗРАБОТКА КОНСТРУКТИВОВ ОБОРУДОВАНИЯ</p>
+                    <div class="hex-content-image">
+                        <img src="/imgs/bkgr_hex_project_oscar_1.jpg" alt="" />
+                    </div>
+                </div>
+                <div class="hex-content-right">
+                    <p class="hex-content-name">РАЗРАБОТАКА ДИЗАЙН-ПРОЕКТА
+                        <br />ФЛАГМАНСКОГО МАГАЗИНА OSCAR;
+                        <br />РАЗРАБОТКА КОНСТРУКТИВОВ ОБОРУДОВАНИЯ</p>
+                    <p class="hex-content-description">Включает разработку и визуализацию следующих составляющих:</p>
+                    <ul class="hex-content-list">
+                        <li>ПРЕ-КОНЦЕПЦИЯ/ ЭСКИЗНЫЙ ПРОЕКТ (в двух вариантах)
+                          <ul>
+                            <li>Аудит типового магазина, ассорт. структуры коллекций и данных сезонных продаж  для определения оптимального зонирования (приоритет и площадь по линиям) флагмана</li>
+                            <li>Общая стилистика торговой концепции </li>
+                            <li>Элементы отделки и декорирования (общие предложения )</li>
+                            <li>Идеи по визуальному зонированию основных торговых зон</li>
+                            <li>Стилистика фасада, витрин и входной группы</li>
+                            <li>Идеи по торговому и неторговому оборудованию</li>
+                            <li>Идеи по уникальному почерку бренда</li>
+                          </ul>
+                        </li>
+                        <li>ПОЛНЫЙ ДИЗАЙН-ПРОЕКТ
+                            <ul>
+                                <li>Визуализация магазина в 3D</li>
+                                <li>План отделки</li>
+                                <li>План расстановки оборудования с размерами</li>
+                                <li>Развертки по стенам</li>
+                                <li>План потолка (с расстановкой размерами и наименованием осветительного оборудования)</li>
+                                <li>План полов (с размерами и наименованием отделочных материалов)</li>
+                                <li>Дизайн-проект фасада и вывески </li>
+                                <li>Дизайн- проект витрин с разработкой конструктивов внесезонных витринных элементов </li>
+                                <li>Дизайн-проект кассовой зоны </li>
+                                <li>Дизайн-проект торгового оборудования и зон примерочных; </li>
+                                <li>Разработка конструктивов нетипового оборудования</li>
+                                <li>Подбор декоративных и  ВМ элементов (манекены, торсы, уникальные деко. элементы, POS материалы)</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </article>
+
+        </section>
+
+        <section class="project-details">
+
+            <article class="hex-content clearfix">
+                <div class="hex-content-left">
+                    <p class="hex-content-name">СЕЗОННЫЙ РЕМЕРЧАНДАЙЗИНГ
+                        <br />ФЛАГМАНСКОГО МАГАЗИНА И ОБУЧЕНИЕ</p>
+                    <div class="hex-content-image">
+                        <img src="/imgs/bkgr_hex_project_oscar_2.jpg" alt="" />
+                    </div>
+                </div>
+                <div class="hex-content-right">
+                    <p class="hex-content-name">СЕЗОННЫЙ РЕМЕРЧАНДАЙЗИНГ
+                        <br />ФЛАГМАНСКОГО МАГАЗИНА И ОБУЧЕНИЕ</p>
+                    <p class="hex-content-description">Включает в себя:</p>
+                    <ul class="hex-content-list">
+                        <li>Полный ремерчандайзинг торгового зала и витрин</li>
+                        <li>Разработка и воплощение сезонной декоративной концепции</li>
+                        <li>Разработка/заказ сезонных деко. элементов </li>
+                        <li>Стайлинг манекенов, торсов, презентации товара</li>
+                        <li>Обучение персонала бренда ВМ стандартам презентации коллекций «под бренд» </li>
+                    </ul>
+                </div>
+            </article>
+
+        </section>
+
+        <section class="new-section">
+          <div class="section-title">Итоги проекта</div>
+        </section>
+        <section class="project-details">
+
+            <div class="project-carrusel project-carrusel-main">
+                <img class="btn-arrows btn-arrows-left" src="/imgs/btn_arrow_left.png" />
+
+                <div class="js-main-carrusel">
+                    <div class="carr-item"><img src="/imgs/projects/oscar-1-01.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/oscar-1-02.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/oscar-1-03.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/oscar-1-04.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/oscar-1-05.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/oscar-1-06.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/oscar-1-07.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/oscar-1-08.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/oscar-1-09.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/oscar-1-10.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/oscar-1-11.jpg" alt=""></div>
+                    <div class="carr-item"><img src="/imgs/projects/oscar-1-12.jpg" alt=""></div>
+                </div>
+
+                <img class="btn-arrows btn-arrows-right" src="/imgs/btn_arrow_right.png" />
+            </div>
+
+            <div class="carrusel-main-legend">/ проект реализован: декабрь 2017 – сентябрь 2018 /</div>
+        </section>
+
+        <!-- <section class="new-section">
+          <div class="section-title">Отзыв клиента</div>
+        </section>
+        <section class="project-testimonials">
+            <div class="person">
+                <img src="/imgs/projects/incanto-testimonial.jpg" alt="Марина Афанасьева">
+                <div class="person-text-layout">
+                    <h4 class="person-name">МАРИНА АФАНАСЬЕВА</h4>
+                    <span class="person-position bkgr-hi">Руководитель Отдела Мерчандайзинга
+                    <br/>«INCANTO Fashion Group»</span>
+                </div>
+            </div>
+            <div class="testimonial-text">
+                <p>Видео магазина и видео отзыв Владимира Болдышева можно посмотреть на канале IGTV в инстаграме агентства @VM_guru</p>
+            </div>
+        </section> -->
+
+        <!-- FOOTER -->
+        <?php include "../footer.php"; ?>
+
+    </div>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+<script src="/js/main.js" type="text/javascript" charset="utf-8"></script>
+
+</html>
