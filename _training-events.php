67f04a00ab484a1210839af59120fac46255c374
Quique Borredá <qborreda@gmail.com>
2018-10-28 11:09:29 +0400

    Changes in Windows training

--- _training-events.php
+++ _training-events.php
@@ -28,20 +28,6 @@
       <div class="cta-button">Узнать больше ›</div>
     </a>
     </article>
-    <!-- WINDOWS -->
-    <article class="container">
-        <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php">
-        <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event4.jpg" class="lazy">
-        <div class="article-stripe">
-            <div class="article-dates">
-                <span class="month">ноября</span>
-                <span class="date">8 - 9</span>
-            </div>
-            <p class="description">Витринистика: создание эффективных витрин. Большой эффект без больших расходов</p>
-        </div>
-        <div class="cta-button">Узнать больше ›</div>
-        </a>
-    </article>
     <!-- SHOES -->
     <article class="container">
         <a class="no-border" href="/visual-merchandising-dlia-magazinov-obuvi-training.php">
@@ -52,6 +38,20 @@
                 <span class="date">15 - 16</span>
             </div>
             <p class="description">Визуальный мерчандайзинг для магазинов обуви и аксессуаров</p>
+        </div>
+        <div class="cta-button">Узнать больше ›</div>
+        </a>
+    </article>
+    <!-- WINDOWS -->
+    <article class="container">
+        <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php">
+        <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event4.jpg" class="lazy">
+        <div class="article-stripe">
+            <div class="article-dates">
+                <span class="month">ноября</span>
+                <span class="date">22 - 23</span>
+            </div>
+            <p class="description">Витринистика: создание эффективных витрин. Большой эффект без больших расходов</p>
         </div>
         <div class="cta-button">Узнать больше ›</div>
         </a>
