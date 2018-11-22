9767e331d99c3642b2ef75c384303b40409b2202
Quique Borredá <qborreda@gmail.com>
2018-11-22 10:56:27 +0300

    Changes to training sections

--- training.php
+++ training.php
@@ -51,42 +51,30 @@
         </section>
 
         <section class="new-section">
-          <div class="section-title">Групповые тренинги</div>
-          <div class="section-intro-text">
-              <p>Oбучение в формате интерактивного тренинга-практикума в группе до 14 человек по утвержденной программе в нашем офисе и на нашей торговой площадке.</p>
-          </div>
-        </section>
-        <section class="trainings-section2">
-          <!-- <div class="trainings-intro">Oбучение в формате интерактивного тренинга-практикума в группе до 14 человек по утвержденной программе в нашем офисе и на нашей торговой площадке.</div> -->
-          <div class="" style="max-width: 700px; margin: 0 auto 20px; text-align: center;">
-              <p style="padding-left: 20px; text-align: center;">При информационной поддержке</p>
-              <img src="/imgs/logo-newretail.jpg" alt="">
-              <img src="/imgs/logo-fashionunited.jpg" alt="">
-          </div>
-          <?php include "_training-events.php"; ?>
-        </section>
-
-        <section class="new-section">
           <div class="section-title">Корпоративное обучение</div>
+          <div class="section-intro-text">Проводит эксперт-практик европейского уровня Анна Баландина.
+              <br />Получить консультацию и заказать обучение для своего бренда вы можете у нас в любое время.
+              <br />Основные форматы:</div>
         </section>
         <section class="trainings-section1">
             <article class="hex-content clearfix">
                 <div class="hex-content-left">
-                    <p class="hex-content-name">ФОРМАТЫ
+                    <!-- <p class="hex-content-name">ФОРМАТЫ
                         <br/>КОРПОРАТИВНОГО
-                        <br/>ОБУЧЕНИЯ</p>
+                        <br/>ОБУЧЕНИЯ</p> -->
                     <div class="hex-content-image">
                         <img src="imgs/trainings-img1.jpg" alt="" />
                     </div>
                 </div>
                 <div class="hex-content-right">
-                    <p class="hex-content-name">ФОРМАТЫ
+                    <!-- <p class="hex-content-name">ФОРМАТЫ
                         <br/>КОРПОРАТИВНОГО
-                        <br/>ОБУЧЕНИЯ</p>
+                        <br/>ОБУЧЕНИЯ</p> -->
                     <p class="hex-content-description" style="margin-bottom: 10px;"><span class="bkgr-hi">РЕМЕРЧАНДАЙЗИНГ МАГАЗИНА С ПРАКТИЧЕСКИМ ОБУЧЕНИЕМ:</span></p>
-                    <p class="hex-content-paragraph">В основе обучения – ремерчандайзинг торгового зала и витрин магазина Заказчика экспертом VM Guru. Сопровождается практическим тренингом персонала бренда с отработкой приемов ВМ «под бренд».</p>
+                    <p class="hex-content-paragraph">В основе обучения – ремерчандайзинг торгового зала и витрин магазина Заказчика.
+                        <br />Сопровождается практическим тренингом персонала бренда с отработкой приемов ВМ «под бренд».</p>
                     <p class="hex-content-paragraph">Ремерчандайзинг осуществляется после экспертного ВМ аудита на основе сводных данных продаж коллекций по линиям, с учетом приоритетной презентации моделей сезонных хитов и драйверов объема.
-                    <br/><strong>Форматы:</strong>  два дня и экспресс (1 день).</p>
+                    <br/><br /><strong>Форматы:</strong>  два дня и экспресс (1 день).</p>
 
                     <p class="hex-content-description" style="margin: 30px 0 10px 0;"><span class="bkgr-hi">ВМ ТРЕНИНГ ИЛИ СИСТЕМА ТРЕНИНГОВ ПОД ЗАПРОС</span></p>
                     <p class="hex-content-paragraph">Структура обучения разрабатывается под запрос Заказчика.
@@ -97,7 +85,6 @@
                       <li>консалтинг клиента по его конкретным ВМ задачам</li>
                     </ul>
                     <p class="hex-content-paragraph" style="margin-bottom: 20px;"><strong>Форматы:</strong> oдин день и более; возможно сезонное обучение.</p>
-                    <p class="hex-content-paragraph">ВМ обучение для своего бренда вы можете заказать у нас в любое время. Повысьте эффективность своих магазинов!</p>
                 </div>
             </article>
 
@@ -118,6 +105,22 @@
                     </ul>
                 </div>
             </article> -->
+        </section>
+
+        <section class="new-section">
+          <div class="section-title">Групповые тренинги</div>
+          <div class="section-intro-text">
+              <p>Oбучение в формате интерактивного тренинга-практикума в группе до 14 человек по утвержденной программе в нашем офисе и на нашей торговой площадке.</p>
+          </div>
+        </section>
+        <section class="trainings-section2">
+          <!-- <div class="trainings-intro">Oбучение в формате интерактивного тренинга-практикума в группе до 14 человек по утвержденной программе в нашем офисе и на нашей торговой площадке.</div> -->
+          <div class="" style="max-width: 700px; margin: 0 auto 20px; text-align: center;">
+              <p style="padding-left: 20px; text-align: center;">При информационной поддержке</p>
+              <img src="/imgs/logo-newretail.jpg" alt="">
+              <img src="/imgs/logo-fashionunited.jpg" alt="">
+          </div>
+          <?php include "_training-events.php"; ?>
         </section>
 
         <section class="new-section">
