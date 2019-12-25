a61d1bafa07da91d8270e316b5daa2286942f2ef
Quique Borredá <qborreda@gmail.com>
2019-12-25 14:35:59 +0100

    Updates one day training dates / programme / price

--- _training-events.php
+++ _training-events.php
@@ -1,5 +1,20 @@
 <!-- TRAINING EVENTS -->
 <div class="skewed-items">
+    <!-- SHOPS -->
+    <article class="container">
+        <a class="no-border" href="/sozdanie-magazine-fashion-segments-training.php">
+        <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event1.jpg" class="lazy">
+        <div class="article-stripe">
+            <div class="article-dates">
+                <span class="month">февраля</span>
+                <span class="date">28</span>
+            </div>
+            <p class="description">ЭФФЕКТИВНЫЙ FASHION-МАГАЗИН.<br />
+                Создайте торговое пространство, которое продает само</p>
+        </div>
+        <div class="cta-button">Узнать больше ›</div>
+        </a>
+    </article>
     <!-- VM -->
     <article class="container">
         <a class="no-border" href="/kommercheskii-visual-merchandising-training.php">
@@ -38,20 +53,6 @@
                 <span class="date">19 - 20</span>
             </div>
             <p class="description">Визуальный мерчандайзинг для магазинов обуви и аксессуаров</p>
-        </div>
-        <div class="cta-button">Узнать больше ›</div>
-        </a>
-    </article>
-    <!-- SHOPS -->
-    <article class="container">
-        <a class="no-border" href="/sozdanie-magazine-fashion-segments-training.php">
-        <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event1.jpg" class="lazy">
-        <div class="article-stripe">
-            <div class="article-dates">
-                <span class="month">февраль</span>
-                <span class="date">-</span>
-            </div>
-            <p class="description">Как создать эффективный фэшн магазин: планировки, зонирование, расчет вместимости, ВМ-дизайн</p>
         </div>
         <div class="cta-button">Узнать больше ›</div>
         </a>
