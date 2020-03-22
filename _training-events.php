87cf532286dbe0d8b078fc0d3d2fd67a1caaf581
Quique Borredá <qborreda@gmail.com>
2020-03-22 20:50:22 +0300

    Reorders training sessions

--- _training-events.php
+++ _training-events.php
@@ -1,33 +1,5 @@
 <!-- TRAINING EVENTS -->
 <div class="skewed-items">
-    <!-- VM -->
-    <article class="container">
-        <a class="no-border" href="/kommercheskii-visual-merchandising-training.php">
-        <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event2.jpg" class="lazy">
-        <div class="article-stripe">
-            <div class="article-dates">
-                <span class="month">марта</span>
-                <span class="date">26 - 27</span>
-            </div>
-            <p class="description">Коммерческий визуальный мерчандайзинг; прикладные техники «от А до Я»</p>
-        </div>
-      <div class="cta-button">Узнать больше ›</div>
-    </a>
-    </article>
-    <!-- WINDOWS -->
-    <article class="container">
-        <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php">
-        <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event4.jpg" class="lazy">
-        <div class="article-stripe">
-            <div class="article-dates">
-                <span class="month">марта</span>
-                <span class="date">30 - 31</span>
-            </div>
-            <p class="description"><strong class="is-cyan">NEW!</strong> Витринистика: создание эффективных витрин. Большой эффект без больших расходов</p>
-        </div>
-        <div class="cta-button">Узнать больше ›</div>
-        </a>
-    </article>
     <!-- SHOES -->
     <article class="container">
         <a class="no-border" href="/visual-merchandising-dlia-magazinov-obuvi-training.php">
@@ -53,6 +25,34 @@
             </div>
             <p class="description">ЭФФЕКТИВНЫЙ FASHION-МАГАЗИН.<br />
                 Создайте торговое пространство, которое продает само</p>
+        </div>
+        <div class="cta-button">Узнать больше ›</div>
+        </a>
+    </article>
+    <!-- VM -->
+    <article class="container">
+        <a class="no-border" href="/kommercheskii-visual-merchandising-training.php">
+        <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event2.jpg" class="lazy">
+        <div class="article-stripe">
+            <div class="article-dates">
+                <span class="month">марта</span>
+                <span class="date">26 - 27</span>
+            </div>
+            <p class="description">Коммерческий визуальный мерчандайзинг; прикладные техники «от А до Я»</p>
+        </div>
+      <div class="cta-button">Узнать больше ›</div>
+    </a>
+    </article>
+    <!-- WINDOWS -->
+    <article class="container">
+        <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php">
+        <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event4.jpg" class="lazy">
+        <div class="article-stripe">
+            <div class="article-dates">
+                <span class="month">марта</span>
+                <span class="date">30 - 31</span>
+            </div>
+            <p class="description"><strong class="is-cyan">NEW!</strong> Витринистика: создание эффективных витрин. Большой эффект без больших расходов</p>
         </div>
         <div class="cta-button">Узнать больше ›</div>
         </a>
