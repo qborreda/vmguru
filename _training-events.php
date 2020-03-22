62ecfd6d089b55373204db1e89353ebfae15fe9f
Quique Borredá <qborreda@gmail.com>
2020-03-22 20:42:22 +0300

    Updates dates for SHOPS training

--- _training-events.php
+++ _training-events.php
@@ -1,19 +1,5 @@
 <!-- TRAINING EVENTS -->
 <div class="skewed-items">
-    <!-- SHOES -->
-    <article class="container">
-        <a class="no-border" href="/visual-merchandising-dlia-magazinov-obuvi-training.php">
-        <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event3.jpg" class="lazy">
-        <div class="article-stripe">
-            <div class="article-dates">
-                <span class="month">онлайн</span>
-                <span class="date">01 апреля</span>
-            </div>
-            <p class="description">Визуальный мерчандайзинг для магазинов обуви и аксессуаров</p>
-        </div>
-        <div class="cta-button">Узнать больше ›</div>
-        </a>
-    </article>
     <!-- VM -->
     <article class="container">
         <a class="no-border" href="/kommercheskii-visual-merchandising-training.php">
@@ -28,21 +14,6 @@
       <div class="cta-button">Узнать больше ›</div>
     </a>
     </article>
-    <!-- SHOPS -->
-    <article class="container">
-        <a class="no-border" href="/sozdanie-magazine-fashion-segments-training.php">
-        <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event1.jpg" class="lazy">
-        <div class="article-stripe">
-            <div class="article-dates">
-                <span class="month">марта</span>
-                <span class="date">28</span>
-            </div>
-            <p class="description">ЭФФЕКТИВНЫЙ FASHION-МАГАЗИН.<br />
-                Создайте торговое пространство, которое продает само</p>
-        </div>
-        <div class="cta-button">Узнать больше ›</div>
-        </a>
-    </article>
     <!-- WINDOWS -->
     <article class="container">
         <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php">
@@ -53,6 +24,35 @@
                 <span class="date">30 - 31</span>
             </div>
             <p class="description"><strong class="is-cyan">NEW!</strong> Витринистика: создание эффективных витрин. Большой эффект без больших расходов</p>
+        </div>
+        <div class="cta-button">Узнать больше ›</div>
+        </a>
+    </article>
+    <!-- SHOES -->
+    <article class="container">
+        <a class="no-border" href="/visual-merchandising-dlia-magazinov-obuvi-training.php">
+        <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event3.jpg" class="lazy">
+        <div class="article-stripe">
+            <div class="article-dates">
+                <span class="month">онлайн</span>
+                <span class="date">01 апреля</span>
+            </div>
+            <p class="description">Визуальный мерчандайзинг для магазинов обуви и аксессуаров</p>
+        </div>
+        <div class="cta-button">Узнать больше ›</div>
+        </a>
+    </article>
+    <!-- SHOPS -->
+    <article class="container">
+        <a class="no-border" href="/sozdanie-magazine-fashion-segments-training.php">
+        <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event1.jpg" class="lazy">
+        <div class="article-stripe">
+            <div class="article-dates">
+                <span class="month">онлайн</span>
+                <span class="date">03 апреля</span>
+            </div>
+            <p class="description">ЭФФЕКТИВНЫЙ FASHION-МАГАЗИН.<br />
+                Создайте торговое пространство, которое продает само</p>
         </div>
         <div class="cta-button">Узнать больше ›</div>
         </a>
