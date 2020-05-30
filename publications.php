46aa0908ea16a2464d4d9107a2e4eb6730774f32
Quique Borredá <qborreda@gmail.com>
2020-05-30 15:02:20 +0300

    Setting up the grid elements + some text

--- publications.php
+++ publications.php
@@ -28,6 +28,52 @@
     <section class="new-section">
       <div class="section-title">Наши публикации 2017 - 2020</div>
     </section>
+
+    <section class="publication-highlights">
+      <div class="highlights">
+        <div class="grid-container">
+          <div class="main">
+            <img class="c-img" src="https://dummyimage.com/300/000/cc31e0.png&text=main" />
+            <div class="c-text">
+              <div class="title">Как не потерять контроль над бизнесом после пандемии. История VM Guru</div>
+              <div class="text"><p>Для перестройки бизнеса под новую реальность важны будут: онлайн формат, оптимизация бизнеса, сохранение команды. Один в поле не воин! О том как мы трансформировали бизнес за 14 дней.</p></div>
+            </div>
+          </div>
+          <div class="hor div2">
+            <img class="c-img" src="https://dummyimage.com/240x150/000/cc31e0.png&text=image" />
+            <div class="c-text">
+              <div class="title">Вернуться ли покупатели в офлайн? мнение Анны Баландиной</div>
+              <div class="text">
+                <p>Люди будут по-прежнему будут предпочитать покупать у людей, а не у роботов. В онлайн бесконечный ассортимент, но нет эмоций от соприкосновения с вещью, да и все эти заморочки с доставкой.</p>
+                <!-- <p>Но магазины должны измениться. Не актуальны будут магазины со «статичной картинкой», построил и не меняешь пространство, пока не «износится» оборудование, такого больше не будет.</p> -->
+              </div>
+            </div>
+          </div>
+          <div class="hor div3">
+            <img class="c-img" src="https://dummyimage.com/240x150/000/cc31e0.png&text=image" />
+            <div class="c-text">
+              <div class="title">Матрица: перезагрузка</div>
+              <div class="text">
+                <p>О главных трендах fashion рынка в 2020:
+                  Взрывной рост e-commerce. Расцвет маркетплейсов.
+                  Этичность бизнеса. Рост популярности форматов низких цен.</p>
+              </div>
+            </div>
+          </div>
+          <div class="hor div4">
+            <img class="c-img" src="https://dummyimage.com/240x150/000/cc31e0.png&text=image" />
+            <div class="c-text">
+              <div class="title">Серые мышки больше не работают</div>
+              <div class="text">
+                <p>Анна Баландина, эксперт практик по визуальному мерчандайзингу и дизайну магазинов о том, почему  важно внедрять в розничную торговлю новые технологии визуального мерчандайзина и нейромаркетинга, и как лучше обучать магазины-арендаторов ТРЦ.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
     <section class="publications-section1">
 
       <!-- Vitrina block -->
