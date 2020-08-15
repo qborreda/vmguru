f5bd7b887ed2df3852c4a1046fbcdc0aced27ce7
Quique Borredá <qborreda@gmail.com>
2020-08-15 12:40:07 +0300

    Adds one more training event; Changes dates online trainings

--- _training-events.php
+++ _training-events.php
@@ -1,6 +1,22 @@
 <!-- TRAINING EVENTS -->
 <div class="skewed-items">
 
+  <!-- LINGERIE -->
+  <article class="container">
+    <!-- <a class="no-border" href="/sozdanie-magazine-fashion-segments-training.php"> -->
+    <a class="no-border" href="https://vmguru-online.ru/lingerie" target="_blank">
+      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event5.jpg" class="lazy">
+      <div class="article-stripe">
+        <div class="article-dates">
+          <span class="month">онлайн</span>
+          <span class="date">8 сентября</span>
+        </div>
+        <p class="description">Оформление магазина нижнего белья.<br />
+          ВМ техники увеличения продаж</p>
+      </div>
+      <div class="cta-button">Узнать больше ›</div>
+    </a>
+  </article>
   <!-- VM -->
   <article class="container">
     <!-- <a class="no-border" href="/kommercheskii-visual-merchandising-training.php"> -->
@@ -17,22 +33,6 @@
       <div class="cta-button">Узнать больше ›</div>
     </a>
   </article>
-  <!-- WINDOWS -->
-  <article class="container">
-    <!-- <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php"> -->
-    <a class="no-border" href="https://vmguru-online.ru/vitrinistika" target="_blank">
-      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event4.jpg" class="lazy">
-      <div class="article-stripe">
-        <div class="article-dates">
-          <span class="month">онлайн</span>
-          <span class="date">12 сентября</span>
-        </div>
-        <p class="description">Витринистика: создание эффективных витрин. Большой
-          эффект без больших расходов</p>
-      </div>
-      <div class="cta-button">Узнать больше ›</div>
-    </a>
-  </article>
   <!-- SHOES -->
   <article class="container">
     <!-- <a class="no-border" href="/visual-merchandising-dlia-magazinov-obuvi-training.php"> -->
@@ -41,15 +41,31 @@
       <div class="article-stripe">
         <div class="article-dates">
           <span class="month">онлайн</span>
-          <span class="date">17 сентября</span>
+          <span class="date">18 сентября</span>
         </div>
         <p class="description">Визуальный мерчандайзинг для магазинов обуви и аксессуаров</p>
       </div>
       <div class="cta-button">Узнать больше ›</div>
     </a>
   </article>
-  <!-- SHOPS -->
+  <!-- WINDOWS -->
   <article class="container">
+    <!-- <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php"> -->
+    <a class="no-border" href="https://vmguru-online.ru/vitrinistika" target="_blank">
+      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event4.jpg" class="lazy">
+      <div class="article-stripe">
+        <div class="article-dates">
+          <span class="month">онлайн</span>
+          <span class="date">24 сентября</span>
+        </div>
+        <p class="description">Витринистика: создание эффективных витрин. Большой
+          эффект без больших расходов</p>
+      </div>
+      <div class="cta-button">Узнать больше ›</div>
+    </a>
+  </article>
+  <!-- SHOPS -->
+  <!-- <article class="container">
     <a class="no-border" href="/sozdanie-magazine-fashion-segments-training.php">
       <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event1.jpg" class="lazy">
       <div class="article-stripe">
@@ -62,5 +78,5 @@
       </div>
       <div class="cta-button">Узнать больше ›</div>
     </a>
-  </article>
+  </article> -->
 </div>
