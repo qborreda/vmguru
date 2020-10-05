919bca80f832b5ad15140b1a359a439f921e54cb
Quique Borredá <qborreda@gmail.com>
2020-10-05 13:23:19 +0300

    Corrects training dates

--- _training-events.php
+++ _training-events.php
@@ -31,22 +31,6 @@
       <div class="cta-button">Узнать больше ›</div>
     </a>
   </article>
-  <!-- WINDOWS -->
-  <article class="container">
-    <!-- <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php"> -->
-    <a class="no-border" href="https://vmguru-online.ru/vitrinistika" target="_blank">
-      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event4.jpg" class="lazy">
-      <div class="article-stripe">
-        <div class="article-dates">
-          <span class="month">онлайн</span>
-          <span class="date">06 октября</span>
-        </div>
-        <p class="description">Витринистика: создание эффективных витрин. Большой
-          эффект без больших расходов</p>
-      </div>
-      <div class="cta-button">Узнать больше ›</div>
-    </a>
-  </article>
   <!-- VM -->
   <article class="container">
     <!-- <a class="no-border" href="/kommercheskii-visual-merchandising-training.php"> -->
@@ -55,10 +39,26 @@
       <div class="article-stripe">
         <div class="article-dates">
           <span class="month">онлайн</span>
-          <span class="date">08 октября</span>
+          <span class="date">06 октября</span>
         </div>
         <p class="description">Визуальный мерчандайзинг.
           <br />Техники увеличения продаж офлайн и онлайн</p>
+      </div>
+      <div class="cta-button">Узнать больше ›</div>
+    </a>
+  </article>
+  <!-- WINDOWS -->
+  <article class="container">
+    <!-- <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php"> -->
+    <a class="no-border" href="https://vmguru-online.ru/vitrinistika" target="_blank">
+      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event4.jpg" class="lazy">
+      <div class="article-stripe">
+        <div class="article-dates">
+          <span class="month">онлайн</span>
+          <span class="date">08 октября</span>
+        </div>
+        <p class="description">Витринистика: создание эффективных витрин. Большой
+          эффект без больших расходов</p>
       </div>
       <div class="cta-button">Узнать больше ›</div>
     </a>
