27d0ad7c71176414ba4fb5707d4869a2c820cda2
Quique Borredá <qborreda@gmail.com>
2020-10-31 18:23:03 +0300

    Hides away Seminars from Home page

--- index.php
+++ index.php
@@ -75,10 +75,10 @@
       </section>
 
       <!-- SEMINARS -->
-      <section class="new-section">
+      <!-- <section class="new-section">
         <div class="section-title">Семинары и мастер-классы</div>
-        <?php include "_seminars-masters.php"; ?>
-      </section>
+        <?php /* include "_seminars-masters.php"; */ ?>
+      </section> -->
 
       <!-- MARKETING -->
       <section class="new-section">
