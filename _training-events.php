780b0662578ec49e67c6742047de8b978b876f71
Quique Borredá <qborreda@gmail.com>
2020-10-31 18:15:43 +0300

    Changes on online training dates

--- _training-events.php
+++ _training-events.php
@@ -1,6 +1,36 @@
 <!-- TRAINING EVENTS -->
 <div class="skewed-items">
-
+  <!-- SHOES -->
+  <article class="container">
+    <!-- <a class="no-border" href="/visual-merchandising-dlia-magazinov-obuvi-training.php"> -->
+    <a class="no-border" href="https://vmguru-online.ru/shoes" target="_blank">
+      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event3.jpg" class="lazy">
+      <div class="article-stripe">
+        <div class="article-dates">
+          <span class="month">онлайн</span>
+          <span class="date">02 декабря</span>
+        </div>
+        <p class="description">Визуальный мерчандайзинг для магазинов обуви и аксессуаров</p>
+      </div>
+      <div class="cta-button">Узнать больше ›</div>
+    </a>
+  </article>
+  <!-- WINDOWS -->
+  <article class="container">
+    <!-- <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php"> -->
+    <a class="no-border" href="https://vmguru-online.ru/vitrinistika" target="_blank">
+      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event4.jpg" class="lazy">
+      <div class="article-stripe">
+        <div class="article-dates">
+          <span class="month">онлайн</span>
+          <span class="date">23 декабря</span>
+        </div>
+        <p class="description">Витринистика: создание эффективных витрин. Большой
+          эффект без больших расходов</p>
+      </div>
+      <div class="cta-button">Узнать больше ›</div>
+    </a>
+  </article>
   <!-- LINGERIE -->
   <article class="container">
     <a class="no-border" href="https://vmguru-online.ru/lingerie" target="_blank">
@@ -16,37 +46,6 @@
       <div class="cta-button">Узнать больше ›</div>
     </a>
   </article>
-  <!-- SHOES -->
-  <article class="container">
-    <!-- <a class="no-border" href="/visual-merchandising-dlia-magazinov-obuvi-training.php"> -->
-    <a class="no-border" href="https://vmguru-online.ru/shoes" target="_blank">
-      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event3.jpg" class="lazy">
-      <div class="article-stripe">
-        <div class="article-dates">
-          <span class="month">онлайн</span>
-          <span class="date">02 ноября</span>
-        </div>
-        <p class="description">Визуальный мерчандайзинг для магазинов обуви и аксессуаров</p>
-      </div>
-      <div class="cta-button">Узнать больше ›</div>
-    </a>
-  </article>
-  <!-- WINDOWS -->
-  <article class="container">
-    <!-- <a class="no-border" href="/vitrinistika-texniki-sozdania-kommercheskix-vitrin.php"> -->
-    <a class="no-border" href="https://vmguru-online.ru/vitrinistika" target="_blank">
-      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event4.jpg" class="lazy">
-      <div class="article-stripe">
-        <div class="article-dates">
-          <span class="month">онлайн</span>
-          <span class="date">10 ноября</span>
-        </div>
-        <p class="description">Витринистика: создание эффективных витрин. Большой
-          эффект без больших расходов</p>
-      </div>
-      <div class="cta-button">Узнать больше ›</div>
-    </a>
-  </article>
   <!-- VM -->
   <article class="container">
     <!-- <a class="no-border" href="/kommercheskii-visual-merchandising-training.php"> -->
@@ -55,7 +54,7 @@
       <div class="article-stripe">
         <div class="article-dates">
           <span class="month">онлайн</span>
-          <span class="date">12 ноября</span>
+          <span class="date">Под запрос</span>
         </div>
         <p class="description">Визуальный мерчандайзинг.
           <br />Техники увеличения продаж офлайн и онлайн</p>
