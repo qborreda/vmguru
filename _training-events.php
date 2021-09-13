0877b1e6c34ed491c040327edbce8731434fbde8
Quique Borredá <qborreda@gmail.com>
2021-09-13 12:09:56 +0300

    Changes order of items in Events

--- _training-events.php
+++ _training-events.php
@@ -1,35 +1,5 @@
 <!-- TRAINING EVENTS -->
 <div class="skewed-items">
-  <!-- LINGERIE -->
-  <article class="container">
-    <a class="no-border" href="https://vmguru-online.ru/lingerie" target="_blank">
-      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event5.jpg" class="lazy">
-      <div class="article-stripe">
-        <div class="article-dates">
-          <span class="month">&nbsp;</span>
-          <span class="date">под запрос</span>
-        </div>
-        <p class="description">Оформление магазина нижнего белья.<br />
-          ВМ техники увеличения продаж</p>
-      </div>
-      <div class="cta-button">Узнать больше ›</div>
-    </a>
-  </article>
-  <!-- SHOES -->
-  <article class="container">
-    <!-- <a class="no-border" href="/visual-merchandising-dlia-magazinov-obuvi-training.php"> -->
-    <a class="no-border" href="https://vmguru-online.ru/shoes" target="_blank">
-      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event3.jpg" class="lazy">
-      <div class="article-stripe">
-        <div class="article-dates">
-          <span class="month">&nbsp;</span>
-          <span class="date">под запрос</span>
-        </div>
-        <p class="description">Визуальный мерчандайзинг для магазинов обуви и аксессуаров</p>
-      </div>
-      <div class="cta-button">Узнать больше ›</div>
-    </a>
-  </article>
   <!-- VM -->
   <article class="container">
     <!-- <a class="no-border" href="/kommercheskii-visual-merchandising-training.php"> -->
@@ -59,6 +29,36 @@
         </div>
         <p class="description">Витринистика: создание эффективных витрин. Большой
           эффект без больших расходов</p>
+      </div>
+      <div class="cta-button">Узнать больше ›</div>
+    </a>
+  </article>
+  <!-- LINGERIE -->
+  <article class="container">
+    <a class="no-border" href="https://vmguru-online.ru/lingerie" target="_blank">
+      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event5.jpg" class="lazy">
+      <div class="article-stripe">
+        <div class="article-dates">
+          <span class="month">&nbsp;</span>
+          <span class="date">под запрос</span>
+        </div>
+        <p class="description">Оформление магазина нижнего белья.<br />
+          ВМ техники увеличения продаж</p>
+      </div>
+      <div class="cta-button">Узнать больше ›</div>
+    </a>
+  </article>
+  <!-- SHOES -->
+  <article class="container">
+    <!-- <a class="no-border" href="/visual-merchandising-dlia-magazinov-obuvi-training.php"> -->
+    <a class="no-border" href="https://vmguru-online.ru/shoes" target="_blank">
+      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event3.jpg" class="lazy">
+      <div class="article-stripe">
+        <div class="article-dates">
+          <span class="month">&nbsp;</span>
+          <span class="date">под запрос</span>
+        </div>
+        <p class="description">Визуальный мерчандайзинг для магазинов обуви и аксессуаров</p>
       </div>
       <div class="cta-button">Узнать больше ›</div>
     </a>
