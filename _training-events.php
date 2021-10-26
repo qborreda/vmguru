807c7c35f8afc7a0755b5ea81596f42189f407d6
Quique Borredá <qborreda@gmail.com>
2021-10-26 13:28:16 +0200

    Changes to training dates

--- _training-events.php
+++ _training-events.php
@@ -1,5 +1,20 @@
 <!-- TRAINING EVENTS -->
 <div class="skewed-items">
+  <!-- LINGERIE -->
+  <article class="container">
+    <a class="no-border" href="https://vmguru-online.ru/lingerie" target="_blank">
+      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event5.jpg" class="lazy">
+      <div class="article-stripe">
+        <div class="article-dates">
+          <span class="month">ноября</span>
+          <span class="date">23</span>
+        </div>
+        <p class="description">Оформление магазина нижнего белья.<br />
+          ВМ техники увеличения продаж</p>
+      </div>
+      <div class="cta-button">Узнать больше ›</div>
+    </a>
+  </article>
   <!-- VM -->
   <article class="container">
     <!-- <a class="no-border" href="/kommercheskii-visual-merchandising-training.php"> -->
@@ -28,21 +43,6 @@
         </div>
         <p class="description">Витринистика: создание эффективных витрин. Большой
           эффект без больших расходов</p>
-      </div>
-      <div class="cta-button">Узнать больше ›</div>
-    </a>
-  </article>
-  <!-- LINGERIE -->
-  <article class="container">
-    <a class="no-border" href="https://vmguru-online.ru/lingerie" target="_blank">
-      <img src="imgs/bkgr_skew_placeholder.jpg" data-src="imgs/bkgr_event5.jpg" class="lazy">
-      <div class="article-stripe">
-        <div class="article-dates">
-          <span class="month">&nbsp;</span>
-          <span class="date">под запрос</span>
-        </div>
-        <p class="description">Оформление магазина нижнего белья.<br />
-          ВМ техники увеличения продаж</p>
       </div>
       <div class="cta-button">Узнать больше ›</div>
     </a>
